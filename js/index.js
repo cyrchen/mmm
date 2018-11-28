9c34dbb9dab5a001ee7470394543f6f199ecee1e
cyr <cyrchen@qq.com>
2018-11-28 20:43:15 +0800

    首页页面完成

---
+++ js/index.js
@@ -0,0 +1,27 @@
+$(function () {
+    $.ajax({
+        type: "get",
+        url: "http://192.168.27.61:9090/api/getindexmenu",
+        dataType: "json",
+        success: function (info) {
+            console.log(info);
+            var htmlStr = template("navTpl", info);
+            $(".mm_nav ul").html(htmlStr);
+            $(".more").nextAll().hide();
+        }
+    });
+    $(".mm_nav ul").on("click", ".more", function () {
+        $(".more").nextAll().slideToggle();
+    })
+
+    $.ajax({
+        type: "get",
+        url: "http://192.168.27.61:9090/api/getmoneyctrl",
+        dataType: "json",
+        success: function (info) {
+            console.log(info);
+            var htmlStr = template("disTpl", info);
+            $(".discount_content ul").html(htmlStr);
+        }
+    })
+})
